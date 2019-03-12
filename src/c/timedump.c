66b0adf58446d24570a105964f7059e408bbd71b
crc-32 <thomas.c.england@gmail.com>
2019-03-12 22:29:48 +0000

    Bootloader writing + Remove stm lib

--- src/c/timedump.c
+++ src/c/timedump.c
@@ -4,6 +4,8 @@
 static Window *s_window;
 static TextLayer *s_text_layer;
 SmartstrapServiceId ssid;
+bool writeConfirm = false;
+bool override = false;
 
 void strap_availability_handler(SmartstrapServiceId service_id,
                                        bool is_available) {
@@ -29,6 +31,11 @@ void dump(bool strap) {
   }
 }
 
+bool isRbl(volatile uint8_t *p8) {
+  char rbl[4] = "rbl\0";
+  char check[4] = {p8[0x1A1], p8[0x1A2], p8[0x1A3], p8[0x1A4]};
+  return *rbl == *check;
+}
 
 static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(s_text_layer, "Dumping via logs");
@@ -36,20 +43,58 @@ static void prv_select_click_handler(ClickRecognizerRef recognizer, void *contex
   text_layer_set_text(s_text_layer, "Done");
 }
 
-/*static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Dumping via smartstrap");
+static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  /*text_layer_set_text(s_text_layer, "Dumping via smartstrap");
   dump(true);
-  text_layer_set_text(s_text_layer, "Done");
-}*/
+  text_layer_set_text(s_text_layer, "Done");*/
+  if(writeConfirm)
+  {
+    writeConfirm = false;
+  }
+}
 
-/*static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Down");
-}*/
+static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  if(!writeConfirm)
+  {
+    text_layer_set_text(s_text_layer, "DOWN: CONFIRM");
+    writeConfirm = true;
+    return;
+  }else{
+    text_layer_set_text(s_text_layer, "!!WRITING!!");
+    volatile uint8_t *p8 = (volatile uint8_t *)0x08000000;
+    if(!isRbl(p8) || override) {
+      // Open bootloader bin
+      ResHandle handle = resource_get_handle(RESOURCE_ID_SNOWY_BOOT_RBL);
+      size_t res_size = resource_size(handle);
+      uint8_t *s_buffer = (uint8_t*)malloc(res_size);
+      resource_load(handle, s_buffer, res_size);
+      // Calc crc of bootloader bin
+      //uint32_t crcFile = crc_32(s_buffer, res_size);
+      // Write new bootloader
+      memcpy((void*)p8, (void*)s_buffer, res_size);
+      // Check crc of new bootloader is same
+      //uint32_t crcWrote = crc_32(p8, res_size);
+      //if(crcWrote != crcFile) {
+      if(false) {//TODO
+        text_layer_set_text(s_text_layer, "!!CHECKSUM MISMATCH!!");
+        writeConfirm = false;
+        override = true;
+        return;
+      }else{
+        text_layer_set_text(s_text_layer, "Success!");
+      }
+
+    }else {
+      text_layer_set_text(s_text_layer, "BL is already Rebble!");
+      override = true;
+    }
+  }
+}
 
 static void prv_click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
-  //window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
-  //window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
 }
 
 static void prv_window_load(Window *window) {
@@ -57,7 +102,7 @@ static void prv_window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 
   s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(s_text_layer, "Select: Dump to log");
+  text_layer_set_text(s_text_layer, "Dwn:Write Sel:Dump");
   text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
 }
